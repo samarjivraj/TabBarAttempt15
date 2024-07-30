adab301b2c44f74ba4c2dedb2aa7fdf5f6c2aef0
Samar <samarjivraj@icloud.com>
2024-07-30 13:31:03 +0100

    Initial Commit

---
+++ TabBarAttempt15/TabBarAttempt15App.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarAttempt15App.swift
+//  TabBarAttempt15
+//
+//  Created by Scholar on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabBarAttempt15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
